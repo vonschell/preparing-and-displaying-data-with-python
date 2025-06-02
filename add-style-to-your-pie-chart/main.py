6c10d1ce2b30afdf7d1a2ab57b7124e7b46289ca
vonschell <vonschell02@gmail.com>
2025-06-02 18:44:28 +0200

    Customize pie chart with slice colours and larger title font size

--- add-style-to-your-pie-chart/main.py
+++ add-style-to-your-pie-chart/main.py
@@ -4,8 +4,11 @@ snack_scores = [80,45,34]
 
 slice_labels = ["Chocolate", "Cheese", "Pickles"]
 
+colors = ["D2691E", "#ff7f50", "#8A2BE2"]
+
 plt.pie(snack_scores, labels=slice_labels)
 
-plt.title("Snack Scores")
+plt.title("Snack Scores", fontsize=22)
 
 plt.savefig("snack_scores.png")
+
