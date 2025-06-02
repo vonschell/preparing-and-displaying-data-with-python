91e9abb74b1d3125d69476aaa533587a98eb09c8
vonschell <vonschell02@gmail.com>
2025-06-02 21:31:29 +0200

    Load and convert Titanic datasets into Numpy arrays for analysis

--- combining-data-with-numpy/main.py
+++ combining-data-with-numpy/main.py
@@ -2,10 +2,18 @@ import csv
 import numpy as np
 
 # ADD CODE: Convert titanic1.csv
-
+with open("titanic1.csv", "r") as file:
+  data = csv.reader(file,delimiter=",")
+  headers = next(data)
+  data_list = list(data)
+  titanic_data1 = np.array(data_list)
 
 # ADD CODE: Convert titanic2.csv 
-
+with open("titanic2.csv", "r") as file:
+  data = csv.reader(file,delimiter=",")
+  headers = next(data)
+  data_list = list(data)
+  titanic_data2 = np.array(data_list)
 
 # ADD CODE: Merge two datasets
 
