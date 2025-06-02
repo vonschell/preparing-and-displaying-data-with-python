cc288ca5f2ea5c6756420dbc06ee763da156ed0b
vonschell <vonschell02@gmail.com>
2025-06-02 10:25:30 +0200

    Create initial pie chart visualisation with snack data and labels

--- create-your-first-data-visualization/main.py
+++ create-your-first-data-visualization/main.py
@@ -1,13 +1,16 @@
 # Import matplotlib library here
-
+import matplotlib.pyplot as plt
 # Let's rank some of our favorite snacks
-snack_scores = []
+snack_scores = [70, 54, 20]
 
+slice_labels = ["chocolate", "chips", "jelly beans"]
 # Let's make a pie chart!
 plt.pie()
 
 # Give your pie chart a title in the quotes
-plt.title("")
+plt.title(snack_scores, labels=slice_labels)
 
 # Put the name of your file in the quotes and give it a .png extension
-plt.savefig("")
+plt.title("Frankie's Favorite Snacks")
+
+plt.savefig("frankies_favorite_snacks.png")
