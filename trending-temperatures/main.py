a7b3018af06a917778a708046bd3409464df0164
vonschell <vonschell02@gmail.com>
2025-06-02 19:38:35 +0200

    Add line chart of 2018 average temperatures in North Pole, Alaska with labels, title, and custom colour

--- trending-temperatures/main.py
+++ trending-temperatures/main.py
@@ -2,3 +2,10 @@ import matplotlib.pyplot as plt
 
 month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 temp = [28,32,31,40,45,55,60,65,54,43,34,30]
+
+plt.plot(month, temp, color='darkorange')
+plt.xlabel("Month", fontsize=16)
+plt.ylabel("Temp in Fahrenheit", fontsize=16)
+plt.title("Avg Temperatures for 2018 in North Pole, Alaska", fontsize=20)
+
+plt.savefig("north_pole_temps.png")
