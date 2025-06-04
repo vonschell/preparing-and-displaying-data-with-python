671aa0f8d3d86ac26c64318bd1a4791028d5e408
vonschell <vonschell02@gmail.com>
2025-06-04 17:12:41 +0200

    Extract and analyze HTML elements from Honey Badger article using beautifulSoup

--- make-your-first-webscraper/main.py
+++ make-your-first-webscraper/main.py
@@ -5,3 +5,17 @@ honey_badger = requests.get("https://en.wikipedia.org/wiki/Honey_badger")
 honey_badger.raise_for_status()
 honey_badger_html = honey_badger.text.encode("utf-8")
 honey_badger_soup = BeautifulSoup(honey_badger_html, 'html.parser')
+
+h2 = honey_badger_soup.find_all("h2")
+
+print(len(h2))
+
+for element in h2:
+    print(element)
+    
+links = honey_badger_soup.find_all("a")
+print(links[:5])  # Print the first 5 links
+
+images = honey_badger_soup.find_all("img")
+print(images[-4:])  # Print the last 4 images
+
