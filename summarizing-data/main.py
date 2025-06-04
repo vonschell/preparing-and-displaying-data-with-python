cff7a3a29e0c86e75420bdda3b95a94ce0c1c2c9
vonschell <vonschell02@gmail.com>
2025-06-04 09:22:36 +0200

    Format avergae fare output using f-strings and round() to display mean fares for survivors and n on-survivors rounded to 2 decimal places

--- summarizing-data/main.py
+++ summarizing-data/main.py
@@ -19,5 +19,5 @@ for index in range(0, len(fare)):
   else:
     fare_not_survived.append(fare[index])
 
-print(np.mean(fare_survived))
-print(np.mean(fare_not_survived))
+print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
+print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
