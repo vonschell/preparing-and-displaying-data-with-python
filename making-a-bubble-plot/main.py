6774f9bd2b2977303041a18fb91b86c581c60c3b
vonschell <vonschell02@gmail.com>
2025-06-04 12:20:18 +0200

    Create bubble plot comparing wage and happiness with colour and size based omn Happiness score

--- making-a-bubble-plot/main.py
+++ making-a-bubble-plot/main.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # Standardizes currency to USD values so that we can better compare results
@@ -47,7 +49,11 @@ print("Countries with the lowest average happiness:",
       happiness_average_per_country.nsmallest(10))
 
 # With f-strings:
-print(f"Countries with the highest average wages:{wage_average_per_country.nlargest(10)}")
-print(f"Countries with the highest average happiness:{happiness_average_per_country.nlargest(10)}")
-print(f"Countries with the lowest average wages:{wage_average_per_country.nsmallest(10)}")
-print(f"Countries with the lowest average happiness:{happiness_average_per_country.nsmallest(10)}")
+# print(f"Countries with the highest average wages:{wage_average_per_country.nlargest(10)}")
+# print(f"Countries with the highest average happiness:{happiness_average_per_country.nlargest(10)}")
+# print(f"Countries with the lowest average wages:{wage_average_per_country.nsmallest(10)}")
+# print(f"Countries with the lowest average happiness:{happiness_average_per_country.nsmallest(10)}")
+
+fig = sns.scatterplot(x="Value", y="Happiness score", hue="Happiness score", size="Happiness score", sizes=(20, 180), data=wage_and_happiness)
+
+plt.title("Annual Salary and Happiness")
