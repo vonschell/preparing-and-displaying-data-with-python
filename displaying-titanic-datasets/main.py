25a13ef63d20e94c684fde09d949df0d0343d9a9
vonschell <vonschell02@gmail.com>
2025-06-04 11:25:29 +0200

    Add Titanic scatterplot visualization using Seaborn and Pandas

--- displaying-titanic-datasets/main.py
+++ displaying-titanic-datasets/main.py
@@ -1,3 +1,9 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import Seaborn as sns
+import pandas as pd
+
+dataframe = pd.read_csv('titanic.csv')
+
+sns.scatterplot(x="Age", y="Fare", hue="Survived", data=dataframe)
