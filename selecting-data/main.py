fd58b335c0b78073e9a032923c134e8e1867b7ea
vonschell <vonschell02@gmail.com>
2025-06-04 08:58:57 +0200

    Add logic slice survived and fare columns with NumPy, convert to 1D arrays, and categorize fares by survival status using a for loop

--- selecting-data/main.py
+++ selecting-data/main.py
@@ -8,13 +8,21 @@ with open("titanic.csv", "r") as file:
   data = np.array(data)
   
 
-survived = np.array
-fare = np.array
+survived = np.array(data[:,[0]], dtype=int.flatten())
+fare = np.array(data[:,[7]], dtype=float.flatten())
 
 #empty lists to hold data from  the for loop
 fare_survived = []
 fare_not_survived = []
 
 #ADD CODE: for loop and if/else statements
+for index in range(0, len(fare)):
+  if survived[index] == 1:
+    fare_survived.append(fare[index])
+  else:
+    fare_not_survived.append(fare[index])
 
 #ADD CODE: print lists
+
+print(fare_survived)
+print(fare_not_survived)
