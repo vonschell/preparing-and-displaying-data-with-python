6299d863395a8abd22f4ec360e950c6504374f30
vonschell <vonschell02@gmail.com>
2025-06-04 11:29:41 +0200

    Add scatterplot of Titanic data with labels, hue, and image export

--- displaying-titanic-datasets/main.py
+++ displaying-titanic-datasets/main.py
@@ -7,3 +7,9 @@ import pandas as pd
 dataframe = pd.read_csv('titanic.csv')
 
 sns.scatterplot(x="Age", y="Fare", hue="Survived", data=dataframe)
+
+plt.xlabel("Passenger Age")
+plt.ylabel("Passenger Fare")
+plt.title("Titanic Passenger Age vs Fare")
+
+plt.savefig("titanic.png")
