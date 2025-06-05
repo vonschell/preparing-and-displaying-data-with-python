4b78da948fe1507d340e6ddf04949f79b8f8d63d
vonschell <vonschell02@gmail.com>
2025-06-05 19:14:04 +0200

    Map language codes to full language names for clearer pie chart labels

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -7,6 +7,15 @@ with open("books.csv","r") as datafile:
 
 language_counts = data['language_code'].value_counts().head(5)
 
+#map language codes to full language names in the dataset
+language_names = {
+    "eng": "English",
+    "spa": "Spanish",   
+    "ger": "German",
+    "en-US": "English (US)",
+    "en-GB": "English (UK)",
+}
+
 plt.pie(language_counts, labels=language_counts.index)
 plt.title('What Language Does Your Book Speak?')
 plt.axis("equal")
