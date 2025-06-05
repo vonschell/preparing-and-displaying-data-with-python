6800b77312d6324461129b2587027fbb08d11ee0
vonschell <vonschell02@gmail.com>
2025-06-05 15:54:59 +0200

    Use a for loop with regex re.compile(' [\.(]') and split() to clean and seperate the conservation status category in the data JSON

--- cleaning-data/main.py
+++ cleaning-data/main.py
@@ -3,3 +3,8 @@ import re
 
 with open("data.json", "r") as text:
     data = json.load(text)
+    
+for item in data:
+    item["category"] = re.compile(" [\.(]").split(item["category"])[0]
+    
+    print(data)
