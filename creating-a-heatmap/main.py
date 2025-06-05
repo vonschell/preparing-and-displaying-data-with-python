5134d1e221ece9d5f3d3de54c39b7396129f439e
vonschell <vonschell02@gmail.com>
2025-06-05 15:24:27 +0200

    Create tips_pivoted variable using pivot_table() to reshape the DataFrame with tip values, size as index, and time as columns to handle duplicate values

--- creating-a-heatmap/main.py
+++ creating-a-heatmap/main.py
@@ -4,3 +4,5 @@ import seaborn as sns
 
 with open("tips.csv", "r") as csvfile:
     tips = pd.read_csv(csvfile, delimiter=",")
+    
+tips_pivoted = tips.pivot_table(values="tip", index=["size"], columns=["time"])
