20f7dc8fb6532b1437200e8cd3a5296eb8c52585
vonschell <vonschell02@gmail.com>
2025-06-05 16:01:29 +0200

    Fix KeyError by using correct key name 'Category' (case-sensitive) when cleaning data with regex

--- cleaning-data/main.py
+++ cleaning-data/main.py
@@ -5,6 +5,6 @@ with open("data.json", "r") as text:
     data = json.load(text)
     
 for item in data:
-    item["category"] = re.compile(" [\.(]").split(item["category"])[0]
+    item["Category"] = re.compile(" [\.(]").split(item["Category"])[0]
     
     print(data)
